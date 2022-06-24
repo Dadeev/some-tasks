92a2b342a2f2477e186b7d395c4c914f88f69259
Tima <dadeev.tim@gmail.com>
2022-06-24 10:49:08 +0700

    init: start my first homework

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+type MessageType = {
+    avatar: string,
+    name: string,
+    message: string,
+    time: string
+}
+
+
+const  Message: React.FC<MessageType> = (props) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Message
